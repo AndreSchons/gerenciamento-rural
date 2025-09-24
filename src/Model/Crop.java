2a128680dd5f09e05eff55aa912acc338e527591
Andre Schons <andreferens86@gmail.com>
2025-09-23 22:08:27 -0300

    Correcao das entidades

--- src/Model/Crop.java
+++ src/Model/Crop.java
@@ -2,4 +2,60 @@ package Model;
 
 public class Crop {
 
+    private int cropId;
+    private String cropName;
+    private String variety;
+    private String season; // e.g., "Spring", "Summer", etc.
+    private int expectedGrowthTime; // in days
+
+    public Crop(int cropId, String cropName, String variety, String season, int expectedGrowthTime) {
+        this.cropId = cropId;
+        this.cropName = cropName;
+        this.variety = variety;
+        this.season = season;
+        this.expectedGrowthTime = expectedGrowthTime;
+    }
+
+    public int getCropId() {
+        return cropId;
+    }
+
+    public void setCropId(int cropId) {
+        this.cropId = cropId;
+    }
+
+    public String getCropName() {
+        return cropName;
+    }
+
+    public void setCropName(String cropName) {
+        this.cropName = cropName;
+    }
+
+    public String getVariety() {
+        return variety;
+    }
+
+    public void setVariety(String variety) {
+        this.variety = variety;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public int getExpectedGrowthTime() {
+        return expectedGrowthTime;
+    }
+
+    public void setExpectedGrowthTime(int expectedGrowthTime) {
+        this.expectedGrowthTime = expectedGrowthTime;
+    }
+
+    
+
 }
--- src/Model/Farmland.java
+++ src/Model/Farmland.java
@@ -1,5 +1,71 @@
 package Model;
 
+import java.util.List;
+
 public class Farmland {
 
+    private int farmlandId;
+    private String farmlandName;
+    private double area; // in hectares
+    private Farm farm; // Association with Farm
+    private Crop crop; // Association with Crop
+    private List<AgriculturalInputs> inputs; // List of agricultural inputs used
+
+    public Farmland(int farmlandId, String farmlandName, double area, Farm farm, Crop crop) {
+        this.farmlandId = farmlandId;
+        this.farmlandName = farmlandName;
+        this.area = area;
+        this.farm = farm;
+        this.crop = crop;
+    }
+
+    public int getFarmlandId() {
+        return farmlandId;
+    }
+
+    public void setFarmlandId(int farmlandId) {
+        this.farmlandId = farmlandId;
+    }
+
+    public String getFarmlandName() {
+        return farmlandName;
+    }
+
+    public void setFarmlandName(String farmlandName) {
+        this.farmlandName = farmlandName;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public Farm getFarm() {
+        return farm;
+    }
+
+    public void setFarm(Farm farm) {
+        this.farm = farm;
+    }
+
+    public Crop getCrop() {
+        return crop;
+    }
+
+    public void setCrop(Crop crop) {
+        this.crop = crop;
+    }
+
+    public List<AgriculturalInputs> getInputs() {
+        return inputs;
+    }
+
+    public void setInputs(List<AgriculturalInputs> inputs) {
+        this.inputs = inputs;
+    }
+
+    
 }
